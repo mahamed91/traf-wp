db885a5530a7fe9e82d88473fb844b6602ed88a7
Mahamed Ali <alimahamed1996@gmail.com>
2019-02-01 11:34:35 +0000

    More fixes

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt update && sudo apt upgrade
+sudo apt update && sudo apt upgrade -y
 sudo apt-get install git unzip curl -y > /dev/null
 curl -L get.docker.com | sudo bash
 docker network create web
 mkdir -p /opt/mysql && mkdir -p /opt/www
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-git clone https://github.com/mahamed91/traf-wp.git
+cd /root && git clone https://github.com/mahamed91/traf-wp.git
 cd traf-wp
 docker-compose -f v2.yaml up -d
 # echo Downloading WP
